4e8da0cbf3170b6ea62cb31bbd7a46a45fdbc622
JonDNewman <JonDNewman@users.noreply.github.com>
2022-10-11 14:29:49 -0400

    add via web upload

---
+++ TurnItInCountingStrings.java
@@ -0,0 +1,20 @@
+    import java.util.Arrays;
+    import java.util.Comparator;
+    import java.util.Map;
+    import java.util.function.Function;
+    import java.util.stream.Collectors;
+
+    public class TurnItInCountingStrings
+    {
+        public static void main(String[] args)
+        {
+            String str = args[0];
+            Arrays.asList(str.split("\\p{Punct}| ")).stream().filter(a->!a.equals("")).collect(Collectors.groupingBy(Function
+                    .identity(), Collectors.counting()))
+                    .entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).forEach(
+                            k -> System.out.println(k.getKey() + " " + k.getValue()));
+
+        }
+    }
+
+
